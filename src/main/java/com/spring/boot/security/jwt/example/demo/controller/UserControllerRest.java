3e009d5151cc61ee2d4850e10a61ddda532b4b8a
Alexandr Shevtsov <ashevtsov@luxoft.com>
2019-06-02 18:36:39 +0300

    jQuery and ajax added

--- src/main/java/com/spring/boot/security/jwt/example/demo/controller/UserControllerRest.java
+++ src/main/java/com/spring/boot/security/jwt/example/demo/controller/UserControllerRest.java
@@ -8,6 +8,9 @@ import com.spring.boot.security.jwt.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -29,6 +32,13 @@ public class UserControllerRest {
     static final String USERS_API = "/api/v1/users";
 
     private final UserService userService;
+
+    @LogExecutionTime
+    @GetMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Page<User>> getAll(@PageableDefault(size = 20, sort = "username") Pageable pageable) {
+        return ResponseEntity.ok(userService.findAll(pageable));
+    }
 
     @LogExecutionTime
     @GetMapping("/{username}")
---
+++ src/main/resources/static/js/users.js
@@ -0,0 +1,78 @@
+var usersUrl = "/api/v1/users";
+
+$(function () {
+    $('body').on('click', '.delete-user', function () {
+        deleteUser($(this).closest('tr'));
+    });
+});
+
+function updateUserList() {
+    $.getJSON(usersUrl, function (data) {
+        $('#user-list').html('');
+        if (data.empty) {
+            $('.user-table').hide();
+            $('#empty-list').show();
+        } else {
+            $.each(data.content, function (index, user) {
+                $('#user-list').append(getUserData(user));
+            });
+            $('.user-table').show();
+            $('#empty-list').hide();
+        }
+    });
+}
+
+function getRoleNames(roles) {
+    var roleNames = [];
+    $.each(roles, function (index, role) {
+        roleNames.push(role.name);
+    });
+    return roleNames.join(', ');
+}
+
+function getUpdateButton(user) {
+    return $('<a class="btn btn-info btn-sm text-white update-user">Update</a>')
+        .attr('href', "/users/edit?userId=" + user.id);
+}
+
+function getDeleteButton(user) {
+    return $('<a class="btn btn-danger btn-sm ml-1 text-white delete-user">Delete</a>');
+}
+
+function getActionButtons(user) {
+    var warnMessage = "Вы уверены что хотите удалить пользователя " + user.username + "?";
+    return $('<td class="text-nowrap"></td>')
+        .append(getUpdateButton(user))
+        .append(getDeleteButton(user));
+}
+
+function getUserData(user) {
+    var userId = user.id;
+    var username = user.username;
+    var password = user.password;
+    var active = user.active;
+    var roles = getRoleNames(user.roles);
+    var actionButtons = getActionButtons(user);
+
+    return $('<tr></tr>')
+        .append($('<td class="username"></td>').text(username))
+        .append($('<td></td>').text(password))
+        .append($('<td></td>').text(active))
+        .append($('<td class="text-nowrap"></td>').text(roles))
+        .append(actionButtons);
+}
+
+function deleteUser(userRow) {
+    var username = userRow.children('.username').text();
+    if (confirm(`Вы действительно хотите удалить пользователя ${username}?`)) {
+        var deleteUserUrl = usersUrl + `/${username}`;
+        $.ajax(deleteUserUrl, {
+            method: 'DELETE',
+            success: function () {
+                updateUserList();
+            }
+        });
+    } else {
+        return false;
+    }
+}
