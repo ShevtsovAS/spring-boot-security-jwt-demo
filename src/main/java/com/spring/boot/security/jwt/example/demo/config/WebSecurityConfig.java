10d0bfdee29871c5bf9da8562da18d0338aa6e2a
Alexandr Shevtsov <ashevtsov@luxoft.com>
2019-10-30 17:23:15 +0300

    oath2 authorization

--- src/main/java/com/spring/boot/security/jwt/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/spring/boot/security/jwt/example/demo/config/WebSecurityConfig.java
@@ -1,59 +1,21 @@
 package com.spring.boot.security.jwt.example.demo.config;
 
-import com.spring.boot.security.jwt.example.demo.service.impl.JpaUserDetailsService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Optional;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Profile("security")
 @EnableWebSecurity
-@RequiredArgsConstructor
+@EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final JWTProperties jwtProperties;
-    private final JpaUserDetailsService userDetailsService;
-    private final PasswordEncoder bCryptPasswordEncoder;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .addFilter(getJWTAuthenticationFilter())
-                .addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtProperties))
-                // this disables session creation on Spring Security
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().anyRequest().authenticated();
     }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-        return source;
-    }
-
-    private JWTAuthenticationFilter getJWTAuthenticationFilter() throws Exception {
-        JWTAuthenticationFilter jwtAuthenticationFilter = new JWTAuthenticationFilter(jwtProperties);
-        jwtAuthenticationFilter.setAuthenticationManager(authenticationManager());
-        Optional.ofNullable(jwtProperties.getLoginPath()).ifPresent(jwtAuthenticationFilter::setFilterProcessesUrl);
-        return jwtAuthenticationFilter;
-    }
 }
